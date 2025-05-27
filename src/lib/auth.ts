aeece82207294d2bfd34d540957d4b41efb1978f
Jeremy C <berdgarage@gmail.com>
2025-05-27 17:18:00 -0600

    🚪 Auth: Add magic link (email) to GitHub + Google setup

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,23 +1,39 @@
+import { NextAuthOptions, Session } from 'next-auth';
+import { JWT } from 'next-auth/jwt';
 import GitHubProvider from 'next-auth/providers/github';
-import GoogleProvider from 'next-auth/providers/google'; // Optional
+import GoogleProvider from 'next-auth/providers/google';
+import EmailProvider from 'next-auth/providers/email';
 
-export const authOptions = {
+export const authOptions: NextAuthOptions = {
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
     }),
-    // GoogleProvider({
-    //   clientId: process.env.GOOGLE_ID!,
-    //   clientSecret: process.env.GOOGLE_SECRET!,
-    // }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID!,
+      clientSecret: process.env.GOOGLE_SECRET!,
+    }),
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: parseInt(process.env.EMAIL_SERVER_PORT || '587', 10),
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASS,
+        },
+      },
+      from: process.env.EMAIL_FROM,
+    }),
   ],
   pages: {
     signIn: '/signin',
   },
   callbacks: {
-    async session({ session, token }) {
-      session.user.id = token.sub;
+    async session({ session, token }: { session: Session; token: JWT }) {
+      if (session.user) {
+        session.user.id = token.sub;
+      }
       return session;
     },
   },
