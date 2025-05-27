a5b358a2b496b4d0d0eb352b7a8e91d8b6caf925
Jeremy C <berdgarage@gmail.com>
2025-05-26 19:39:45 -0600

    ✅ Refactor: Move authOptions to lib/auth/options

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,5 @@
-import NextAuth, { NextAuthOptions } from 'next-auth';
-import GitHubProvider from 'next-auth/providers/github';
-
-export const authOptions: NextAuthOptions = {
-  providers: [
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID!,
-      clientSecret: process.env.GITHUB_SECRET!,
-    }),
-  ],
-};
+import NextAuth from 'next-auth';
+import { authOptions } from '@/lib/auth/options';
 
 const handler = NextAuth(authOptions);
 
